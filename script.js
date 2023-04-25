3aeaa0e7fadbbbcf14ed1a74084265e0913d17fb
Rosemary Inang <inangrosemary@gmail.com>
2023-04-25 16:23:32 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
+// let navBtn = document.querySelector('.fas');
+// let navWrapper = document.querySelector('#nav-items-wrapper');
+// let navItem = document.querySelectorAll('.item');
+// let body = document.querySelector('body');
+
+// navBtn.addEventListener('click', function() {
+//     setTimeout(function() {
+//         navBtn.classList.toggle("fa-times");
+//     }, 300);
+
+//     if ( navWrapper.style.left != "0px" ) {
+//         navWrapper.style.left = "0px";
+//         body.style.overflowY = 'hidden'
+//     } else {
+//         navWrapper.style.left = "-220px";
+//         body.style.overflowY = 'scroll'
+//     }
+
+//     navItem.forEach( item =>{
+//         item.style.visibility = 'hidden'
+//         setTimeout(()=>{
+//             item.style.visibility = 'visible'
+//         }, 350)
+//        })
+// })
+
 const toggleBtn  = document.querySelector('#top .hamburger');
 const navbar  = document.querySelector('#top ul');
 
